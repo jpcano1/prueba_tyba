76269476f5e0d7eb26db153994135b39c1d4d61a
Juan Pablo <jp.cano@uniandes.edu.co>
2021-12-23 20:31:25 -0500

    Sentry Config

--- base_app/api/base.py
+++ base_app/api/base.py
@@ -3,9 +3,12 @@ File to create base app and other apps
 ======================================
 """
 from http import HTTPStatus
+import os
 
 from flask import Flask
 from flask_cors import CORS
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def init_app() -> Flask:
@@ -20,6 +23,18 @@ def init_app() -> Flask:
         Health check route
         """
         return "Hello World", HTTPStatus.OK
+
+    @flask_app.route("/error")
+    def error() -> tuple[int, int]:
+        return 1 / 0, HTTPStatus.OK
+
+    if flask_app.env == "production":
+        sentry_sdk.init(
+            dsn=os.getenv("SENTRY_DSN"),
+            integrations=[FlaskIntegration()],
+            traces_sample_rate=0.01,
+            environment=flask_app.env,
+        )
 
     CORS(flask_app)
 
